8f84d4d1acbca89c7d547c4445baf814b513e5fd
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-07-04 13:48:29 +0700

    modified/admin products api

--- src/app/api/admin/products/route.ts
+++ src/app/api/admin/products/route.ts
@@ -1,3 +1,4 @@
+import ConnectToDatabase from "@/lib/ConnectToDatabase";
 import Product from "@/models/Product";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -5,12 +6,42 @@ const limit = 5
 
 export async function GET(req:NextRequest) {
 
-    const { searchParams } = new URL(req.url)
-    const page = Number(searchParams.get("page")) || 1
+    try {
+        await ConnectToDatabase()
+        const { searchParams } = new URL(req.url)
+        const page = Number(searchParams.get("page")) || 1
+        
+        const products = await Product.find().limit(limit).skip((page - 1) * limit)
+        const totalProducts = await Product.countDocuments()
+        
+        return NextResponse.json({products, maxPage : Math.ceil(totalProducts / limit)}, { status : 200 })
 
-    const products = await Product.find().limit(limit).skip((page - 1) * limit)
-    const totalProducts = await Product.countDocuments()
+    } catch (error) {
+        console.log("Error : " , error)
+        return NextResponse.json({ message : "Internal server error" }, {status : 500})
+    }
 
-    return NextResponse.json({products, maxPage : Math.ceil(totalProducts / limit)}, { status : 200 })
+}
 
+export async function PUT(req:NextRequest) {
+    try {
+        const { searchParams } = new URL(req.url)
+
+        const productId = searchParams.get("productId")
+        const { name, price, img, stock } = await req.json()
+ 
+        if(!(name && price && img && stock)) return NextResponse.json({ message : "Data invalid" }, { status : 400 })
+
+        const editedProduct = await Product.findByIdAndUpdate(productId, { name, price, img, stock }, {
+            new : true,
+            runValidators : true
+        })
+        if(!editedProduct) return NextResponse.json({ message : "Product not found" }, { status : 404 })
+        
+        return NextResponse.json(editedProduct, { status : 200 })
+
+    } catch (error) {
+        console.log("Error " , error)
+        return NextResponse.json({ message : "Internal server error" }, { status : 500 })
+    }
 }
