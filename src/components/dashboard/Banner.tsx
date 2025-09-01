864d93cbfc6d80aa820d0cc2294ed347f8c2d34e
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-09-01 16:10:41 +0700

    ui/change banner img

--- src/components/dashboard/Banner.tsx
+++ src/components/dashboard/Banner.tsx
@@ -1,35 +1,30 @@
 "use client"
 
 import { Swiper, SwiperSlide } from "swiper/react"
-import { ArrowLeft, ArrowRight } from "lucide-react"
-import { Autoplay, Navigation, Pagination } from "swiper/modules"
+import { Autoplay,  Pagination } from "swiper/modules"
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import Image from "next/image";
 
-const bannerImg:string[] = ["/assets/img/banner1.jpg","/assets/img/banner2.jpg","/assets/img/banner3.jpg",]
+const bannerImg:string[] = ["/assets/img/banner1.webp","/assets/img/banner2.webp","/assets/img/banner3.webp",]
 
 const Banner = () => {   
 
   return (
-    <div className="mt-5 lg:w-[90vw] lg:mt-10 mx-auto  rounded relative ">
-      <button id="prev" className="absolute left-2 lg:-left-10 z-10 bg-black text-white  top-1/2 -translate-y-1/2 lg:p-1 rounded-full hover:bg-black/70 active:bg-black/50"><ArrowLeft  /></button>
-      <button id="next" className="right-2 absolute lg:-right-10 z-10 bg-black text-white  top-1/2 -translate-y-1/2 lg:p-1 rounded-full hover:bg-black/70 active:bg-black/50"><ArrowRight  /></button>
+    <div className="mt-5 lg:mt-10">
       <Swiper
-      modules={[Navigation, Autoplay, Pagination]}
+      modules={[Autoplay, Pagination]}
       spaceBetween={100}
       pagination={{ clickable : true }}
       loop
-      navigation={{
-        prevEl : "#prev",
-        nextEl : "#next"
-      }}
       autoplay={{ delay : 3000, disableOnInteraction : false }}
       speed={800}
+      className="rounded"
       >
         {bannerImg.map((banner, index) => (
           <SwiperSlide key={index}>
-            <div style={{ backgroundImage : `url(${banner})`}} className="overflow-hidden rounded  bg-cover bg-center lg:w-full h-40 lg:h-96 " >
+            <div className="overflow-hidden rounded relative aspect-[5/2]" >
+              <Image src={banner} alt="Banner" fill sizes="100vw" className="object-center object-cover"/> 
             </div>
           </SwiperSlide>
         ))}
