3037f4eddd00ad624936bbdce3a2573982ed8c42
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-09-03 17:26:39 +0700

    fix/update parameter for product by id api

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -2,14 +2,14 @@ import ConnectToDatabase from "@/lib/ConnectToDatabase";
 import Product from "@/models/Product";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest,{ params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, context : { params : { id : string } } ) {
   
-  const { id } = params;
+  const { id } = context.params;
   if (!id) return NextResponse.json({ message: "Id not found", id }, { status: 404 });
 
   try {
     await ConnectToDatabase();
-
+    console.log(req.url)
     const product = await Product.findById(id);
 
     if (!product)
