019230bc5440e5202b5581993567523209580e42
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-09-10 09:59:08 +0700

    ui/update detail product page

--- src/components/detailProductPage/SimilarProducts.tsx
+++ src/components/detailProductPage/SimilarProducts.tsx
@@ -3,10 +3,10 @@ import ListProducts from '../common/ListProducts'
 
 const SimilarProducts = ({ products } : { products : Product[] | undefined }) => {
   return (
-    <>
+    <div className='w-full'>
       <h1 className='text-center font-bold text-xl mb-4'>Similar Products</h1>
       <ListProducts products={products} similar/>
-    </>
+    </div>
   )
 }
 
