16389f616c0627aad5121698791b588af1350332
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-09-16 17:37:49 +0700

    ui/update historyshopping medium large screen (lg) design

--- src/components/profile/HistoryShopping/StatusPembelian.tsx
+++ src/components/profile/HistoryShopping/StatusPembelian.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const StatusPembelian : FC<Props> = ({ paymentMethod, paymentStatus, orderStatus }) => {
   return (
-    <div className="text-sm space-y-2 mt-4 md:text-lg">
+    <div className="text-sm space-y-2 mt-4 md:text-lg lg:text-xl">
         <p className="flex justify-between mb-4">Metode pembayaran : <span>{paymentMethod}</span></p>
         <p className="flex justify-between">Status pembayaran : <span className={`${paymentStatus === "unpaid" ? "bg-yellow-500" : "bg-green-400"} px-4 py-1 rounded text-white font-semibold md:px-8`}>{paymentStatus}</span></p>
         <p className="flex justify-between items-center">Status pengiriman : <span className={`${orderStatus === "pending" ? "bg-yellow-500" : "bg-green-400"} px-4 py-1 rounded text-white font-semibold md:px-8`}>{orderStatus}</span></p>
--- src/components/profile/HistoryShopping/index.tsx
+++ src/components/profile/HistoryShopping/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image"
 import DeskripsiProduct from "./DeskripsiProduct"
 import StatusPembelian from "./StatusPembelian"
 import Link from "next/link"
+import { Clock, ShoppingCart } from "lucide-react"
 
 const HistoryShopping = ({ historyShopping } : { historyShopping : Order[] }) => {
 
@@ -13,12 +14,12 @@ const HistoryShopping = ({ historyShopping } : { historyShopping : Order[] }) =>
     <div className='w-full md:w-[90%] md:mx-auto'>
         {historyShopping?.map(({ orderedProduct : { name, img, price, quantity, productId }, createdAt, totalPrice, paymentMethod, paymentStatus,orderStatus }, index) => (
         <div key={index} className='relative pb-16 mb-10 border-b border-black md:border-b-4 md:pb-20'>
-            <div className="flex justify-between items-center mb-4 text-sm md:text-base md:mb-6">
-                <h1 className='text-gray-800'><FormatedDate date={createdAt}/></h1>
-                <Link href={`/products/${productId}`} className="bg-blue-400 text-white px-4 py-1 rounded font-semibold hover:bg-blue-500 md:px-8 md:py-1.5">Beli lagi</Link>
+            <div className="flex justify-between items-center mb-4 text-sm md:text-base md:mb-6 lg:text-lg">
+                <h1 className='text-gray-400 flex items-center gap-2'><Clock /><FormatedDate date={createdAt}/></h1>
+                <Link href={`/products/${productId}`} className="bg-blue-400 text-white px-4 py-1 rounded font-semibold flex items-center gap-2 hover:bg-blue-500 md:px-8 md:py-1.5 lg:gap-4"><ShoppingCart />Beli lagi</Link>
             </div>
             <div className="flex justify-between">
-                <section className="relative aspect-[1/1] w-40 md:w-72">
+                <section className="relative aspect-[1/1] w-40 md:w-72 lg:w-96">
                     <Image src={img} fill alt={name}/>
                 </section>
                 <DeskripsiProduct name={name} quantity={quantity} price={price}/>
